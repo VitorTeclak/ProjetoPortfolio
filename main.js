0de0762f4d9bb456a7621319aec824cebca59fe9
VitorTeclak <vitorteclak@hotmail.com>
2025-08-24 17:42:48 -0300

    Criando portfolio web

---
+++ main.js
@@ -0,0 +1,12 @@
+const tabClicked = (tab) => {
+    const contents = document.querySelectorAll('.content');
+    contents.forEach(content => content.classList.remove('show'));
+
+    const contentId = tab.getAttribute('content-id');
+    const content = document.getElementById(contentId);
+
+    content.classList.add('show');
+}
+
+const tabs = document.querySelectorAll('.tab-btn');
+tabs.forEach(tab => tab.addEventListener('click', () => tabClicked(tab)));
